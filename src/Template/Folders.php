f5f9aebeb635f37bb20c80b4ac3fe9358809adad
Oleg Kasyanov <dev@mobicms.net>
2019-11-30 13:32:00 +0400

    Source code has been simplified

--- src/Template/Folders.php
+++ src/Template/Folders.php
@@ -30,7 +30,7 @@ class Folders
      */
     public function add(string $name, string $path): self
     {
-        if ($this->exists($name)) {
+        if (isset($this->folders[$name])) {
             throw new LogicException('The template folder "' . $name . '" is already being used.');
         }
 
@@ -46,21 +46,10 @@ class Folders
      */
     public function get(string $name): Folder
     {
-        if (! $this->exists($name)) {
+        if (! isset($this->folders[$name])) {
             throw new LogicException('The template folder "' . $name . '" was not found.');
         }
 
         return $this->folders[$name];
-    }
-
-    /**
-     * Check if a template folder exists
-     *
-     * @param string $name
-     * @return bool
-     */
-    public function exists(string $name): bool
-    {
-        return isset($this->folders[$name]);
     }
 }
